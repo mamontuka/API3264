22cc7af509ed161fa2f0845a3dc53b462e7062f1
Oleh Mamont <mamontuka@gmail.com>
2026-06-04 21:00:51 +0000

    Forgotten fixed

--- handlers/completions.py
+++ handlers/completions.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2026
+## Copyright (C) 2026
 #
 # Authors:
 #
@@ -19,7 +19,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# along with this program. If not, see <https://www.gnu.org/>.
 
 """
 MODULE: HANDLERS COMPLETIONS
--- handlers/selenium_bridge.py
+++ handlers/selenium_bridge.py
@@ -4,6 +4,10 @@
 #
 # Production-grade version by Oleh Mamont - https://github.com/mamontuka
 #
+# Based on:
+# y13sint - https://github.com/y13sint
+# raz0r-code - https://github.com/raz0r-code
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -11,11 +15,11 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# along with this program. If not, see <https://www.gnu.org/>.
 
 """
 MODULE: SELENIUM BRIDGE (Playwright-based)
